dae04d19e5311e4c46e259348585df28242cb2e4
KapitanTobal <161176693+KapitanTobal@users.noreply.github.com>
2026-09-23 20:14:28 -0600

    Finished C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+
+            int guess = -1;
+
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (number > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (number < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it right!");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
